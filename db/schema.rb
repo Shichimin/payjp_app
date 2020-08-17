0b4d1c658d9e1e193f9495044f295dd5e7266f2f
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-17 18:34:06 +0900

    カード登録画面の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_090223) do
+ActiveRecord::Schema.define(version: 2020_08_17_092240) do
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "card_token", null: false
+    t.string "customer_token", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -32,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_08_17_090223) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
